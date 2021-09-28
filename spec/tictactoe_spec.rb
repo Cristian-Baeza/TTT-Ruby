8b21cd2ae3dff5ac598bf36d2f269a0abb4cfc2e
Cristian-Baeza <cristianbaeza092@gmail.com>
2021-09-28 11:26:00 -0600

    PR edit- show board test

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -5,9 +5,12 @@ RSpec.describe Tic_Tac_Toe do
   describe "#show_empty_board" do
     it "shows empty TTT board in terminal" do
       new_game = Tic_Tac_Toe.new
-      row = "   " +  "|" +  "   " + "|" + "   "
-      separator = "-----------"
-      board = row + "\n" + separator + "\n" + row + "\n" + separator + "\n" + row
+      board = ""
+      board += "   |   |   \n"
+      board += "-----------\n"
+      board += "   |   |   \n"
+      board += "-----------\n"
+      board += "   |   |   "
 
       expect(new_game.show_empty_board()).to eq(board)
     end
