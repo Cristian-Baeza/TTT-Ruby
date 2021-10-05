8de8d5ba998631fd1549c2517a22619d5ae5d8ff
Cristian-Baeza <cristianbaeza092@gmail.com>
2021-10-05 12:50:53 -0700

    Displays current players turn

--- lib/questions.rb
+++ lib/questions.rb
@@ -1,11 +1,29 @@
 class Questions
+  attr_reader :player_counter
+
+  def initialize()
+    @player_counter = 2
+  end
+
+  def increase_player_counter
+    @player_counter += 1
+  end
+
+  def current_player()
+    @player_counter.even? ? "PLAYER X" : "PLAYER O"
+  end
+
 
   def validate_user_input(question, valid_options)
-      puts question
+      puts question + current_player()
       user_answer = gets.chomp
-      return user_answer.to_i if valid_options.include?(user_answer)
-      puts "INVALID OPTION"
-      validate_user_input(question, valid_options)
+      if valid_options.include?(user_answer)
+        increase_player_counter()
+        return user_answer.to_i
+      else
+        puts "INVALID OPTION"
+        validate_user_input(question, valid_options)
+      end 
   end
 
 end
--- spec/questions_spec.rb
+++ spec/questions_spec.rb
@@ -1,29 +1,58 @@
 require 'questions'
 
 RSpec.describe Questions do
+describe "#increase_player_counter" do
+  it "increases global variable of player_counter" do
+    questions = Questions.new
+    questions.increase_player_counter
+
+    expect(questions.player_counter).to eq(3)
+  end
+end
+
 describe "#validate_user_input" do
   it "returns the value the user picked and change to integer" do
       questions = Questions.new
       allow(questions).to receive(:gets).and_return("1\n")
 
-      answer = questions.validate_user_input("PICK A SPACE?", ["1", "2", "3", "4", "5", "6", "7", "8", "9"])
+      answer = questions.validate_user_input("PICK A SPACE ", ["1", "2", "3", "4", "5", "6", "7", "8", "9"])
 
       expect(answer).to eq(1)
   end
 
   it "it loops asking the question until it gets a valid answer" do
-      question = "PICK A SPACE?"
+      questions = Questions.new  
+      question = "PICK A SPACE "
       options = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-      questions = Questions.new
+      
 
       allow(questions).to receive(:gets).and_return("10\n", "1\n")
-      expect(questions).to receive(:puts).twice.with(question)
+      expect(questions).to receive(:puts).twice.with(question + questions.current_player())
       expect(questions).to receive(:puts).with("INVALID OPTION")
 
       answer = questions.validate_user_input(question, options)
 
       expect(answer).to eq(1)
   end
+end
 
+describe "#current_player" do
+  it "returns player X if global variable is even" do
+      questions = Questions.new
+      
+      expect(questions.current_player()).to eq("PLAYER X")
   end
+
+  it "returns player O if global variable is odd" do
+      questions = Questions.new
+      questions.increase_player_counter()
+      
+      expect(questions.current_player()).to eq("PLAYER O")
+  end
+end
+
+
+
+
+
 end
