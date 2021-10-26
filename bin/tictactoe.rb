69f5091f12b0d04097da8038c17ec3593fb2e1c1
Cristian-Baeza <cristianbaeza092@gmail.com>
2021-10-26 11:50:33 -0600

    Fix console_output argument according to Tyler fixes

--- bin/tictactoe.rb
+++ bin/tictactoe.rb
@@ -3,7 +3,7 @@ require_relative "../lib/game_loop"
 questions = Questions.new
 game_config = GameConfig.new(questions)
 game_logic = GameLogic.new
-console_output = ConsoleOutput.new
+console_output = ConsoleOutput.new(game_config)
 
 game_loop = GameLoop.new(questions, game_logic, game_config, console_output)
 
