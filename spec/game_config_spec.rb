1f2b56b07d14aa227b0d4f334ac17d62484ce149
Cristian Baeza Ibarra <cristianbaeza092@gmail.com>
2021-10-28 12:07:14 -0700

    game flags work (#22)

--- spec/game_config_spec.rb
+++ spec/game_config_spec.rb
@@ -25,4 +25,28 @@ describe GameConfig do
       expect(game_config.player_two_type).to eq(:computer)
     end
   end
+
+  describe "#set_player_type" do
+    it "sets player_one & player_two type to :human if given hvh flag" do
+      questions = Questions.new
+      parsed_arguments = ArgumentParser.new(["hvh"])
+      allow(questions).to receive(:validate_user_input).and_return(1)
+      game_config = GameConfig.new(questions, parsed_arguments)
+
+      expect(game_config.player_one_type).to eq(:human)
+      expect(game_config.player_two_type).to eq(:human)
+
+    end
+    
+    it "sets player_two type to :computer if given hvc flag" do
+      questions = Questions.new
+      parsed_arguments = ArgumentParser.new(["hvc"])
+      allow(questions).to receive(:validate_user_input).and_return(1)
+      game_config = GameConfig.new(questions, parsed_arguments)
+
+      expect(game_config.player_two_type).to eq(:computer)
+    end
+  end
+
+
 end
