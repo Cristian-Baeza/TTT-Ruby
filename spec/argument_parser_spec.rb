1c8cc1fcbbd11653baf7d5126a4727a069feca9a
Cristian-Baeza <cristianbaeza092@gmail.com>
2021-10-27 19:02:16 -0600

    argument_parser tests pass

--- spec/argument_parser_spec.rb
+++ spec/argument_parser_spec.rb
@@ -1,15 +1,15 @@
 require 'argument_parser'
 
 describe "ArgumentParser" do
-  it "should set parsed values to nil if emptry string is passed in" do
+  it "should set parsed values to nil if empty string is passed in" do
     parsed_arguments = ArgumentParser.new("")
 
     expect(parsed_arguments.game_type[0]).to be nil
   end
   
-  # xit "should set @game_type to values passed in" do
-  #   parsed_arguments = ArgumentParser.new("hvh")
+  it "should set @game_type to values passed in" do
+    parsed_arguments = ArgumentParser.new("hvh")
 
-  #   expect(parsed_arguments.game_type[0]).to eq(["hvh"])
-  # end
+    expect(parsed_arguments.game_type).to eq("hvh")
+  end
 end
