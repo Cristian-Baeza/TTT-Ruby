766fb830f824327fef5f8a4e486954cb650caa36
Cristian Baeza Ibarra <cristianbaeza092@gmail.com>
2021-10-29 12:38:04 -0700

    color flag works (#23)

--- lib/argument_parser.rb
+++ lib/argument_parser.rb
@@ -1,12 +1,29 @@
 class ArgumentParser
-  attr_accessor :game_type
-  @game_type
+  attr_accessor :game_type, :color
+  @game_type = nil
+  @color = nil
 
-    def initialize(game_type_user_chose)
-      if game_type_user_chose[0] == ""
-        @game_type = nil
-      else
-        @game_type = game_type_user_chose
-      end
+    def initialize(config_array)
+      parse_game_type(config_array)
+      parse_color_board(config_array)
     end
-end
+
+    def parse_color_board(config_array)
+      config_array.each {|item|
+        if item == "color"
+          @color = true
+        end
+      }
+    end
+
+    def parse_game_type(config_array)
+      config_array.each {|item|
+        if item == "hvh"
+          @game_type = item
+        elsif item == "hvc"
+          @game_type = item
+        end
+      }
+    end
+
+  end
