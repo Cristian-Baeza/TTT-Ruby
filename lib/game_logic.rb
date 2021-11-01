a3279636bf6b9206abcd4eeac1fa013d7c2c1524
Cristian Baeza Ibarra <cristianbaeza092@gmail.com>
2021-11-01 14:29:36 -0700

    cpu blocks two in a row (#24)

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -58,10 +58,102 @@ class GameLogic
   end
 
   def cpu_turn
-    random_open_space = open_spaces().sample
-    take_turn(random_open_space)
+    if cpu_blocks_two_in_row_horizontally?()
+    elsif cpu_blocks_two_in_row_vertically?()
+    elsif cpu_blocks_two_in_row_diagonally?()
+    else
+      random_open_space = open_spaces().sample
+      take_turn(random_open_space)  
+    end
   end
 
+  def cpu_blocks_two_in_row_horizontally?
+    # horizontal_combos = [
+    # [0,1], #2
+    # [1,2], # 0
+    # [3,4], # 5
+    # [4,5], # 3
+    # [6,7], # 8
+    # [7,8], # 6
+    # ]
+    if @board_spaces[0] == :X && @board_spaces[1] == :X && @board_spaces[2] == :empty
+      @board_spaces[2] = :O
+      true
+    elsif @board_spaces[1] == :X && @board_spaces[2] == :X && @board_spaces[0] == :empty
+      @board_spaces[0] = :O
+      true
+    elsif @board_spaces[3] == :X && @board_spaces[4] == :X && @board_spaces[5] == :empty
+      @board_spaces[5] = :O
+      true
+    elsif @board_spaces[4] == :X && @board_spaces[5] == :X && @board_spaces[3] == :empty
+      @board_spaces[3] = :O
+      true
+    elsif @board_spaces[6] == :X && @board_spaces[7] == :X && @board_spaces[8] == :empty
+      @board_spaces[8] = :O
+      true
+    elsif @board_spaces[7] == :X && @board_spaces[8] == :X && @board_spaces[6] == :empty
+      @board_spaces[6] = :O
+      true
+    else
+      false
+    end
+  end
+
+  def cpu_blocks_two_in_row_vertically?
+    vertical_combos = [
+    [0,3], #6
+    [3,6], # 0
+    [1,4], # 7
+    [4,7], # 1
+    [2,5], # 8
+    [5,8], # 2
+    ]
+    if @board_spaces[0] == :X && @board_spaces[3] == :X && @board_spaces[6] == :empty
+      @board_spaces[6] = :O
+      true
+    elsif @board_spaces[3] == :X && @board_spaces[6] == :X && @board_spaces[0] == :empty
+      @board_spaces[0] = :O
+      true
+    elsif @board_spaces[1] == :X && @board_spaces[4] == :X && @board_spaces[7] == :empty
+      @board_spaces[7] = :O
+      true
+    elsif @board_spaces[4] == :X && @board_spaces[7] == :X && @board_spaces[1] == :empty
+      @board_spaces[1] = :O
+      true
+    elsif @board_spaces[2] == :X && @board_spaces[5] == :X && @board_spaces[8] == :empty
+      @board_spaces[8] = :O
+      true
+    elsif @board_spaces[5] == :X && @board_spaces[8] == :X && @board_spaces[2] == :empty
+      @board_spaces[2] = :O
+      true
+    else
+      false
+    end
+  end
+  
+  def cpu_blocks_two_in_row_diagonally?
+    diagonal_combos = [
+    [0,4], #8
+    [4,8], # 0
+    [2,4], # 6
+    [6,4], # 2
+    ]
+    if @board_spaces[0] == :X && @board_spaces[4] == :X && @board_spaces[8] == :empty
+      @board_spaces[8] = :O
+      true
+    elsif @board_spaces[4] == :X && @board_spaces[8] == :X && @board_spaces[0] == :empty
+      @board_spaces[0] = :O
+      true
+    elsif @board_spaces[2] == :X && @board_spaces[4] == :X && @board_spaces[6] == :empty
+      @board_spaces[6] = :O
+      true
+    elsif @board_spaces[6] == :X && @board_spaces[4] == :X && @board_spaces[2] == :empty
+      @board_spaces[2] = :O
+      true
+    else
+      false
+    end
+  end
 
 
 end
