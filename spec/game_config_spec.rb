428361e9eb8d3b33233164b9639a8d00d15a8501
Cristian Baeza Ibarra <cristianbaeza092@gmail.com>
2021-11-10 07:35:37 -0800

    player 1 can now be cpu (#32)

--- spec/game_config_spec.rb
+++ spec/game_config_spec.rb
@@ -14,14 +14,14 @@ describe GameConfig do
       expect(game_config.player_two_type).to eq(:human)
     end
 
-    it 'when user picks 2 cpu for player 2 both players are human' do
+    it 'when user picks 2 cpu for player 2 both players are computer' do
       questions = Questions.new
       parsed_env_vars = EnvVarParser.new()
 
       allow(questions).to receive(:validate_user_input).and_return(2)
       game_config = GameConfig.new(questions, parsed_env_vars)
 
-      expect(game_config.player_one_type).to eq(:human)
+      expect(game_config.player_one_type).to eq(:computer)
       expect(game_config.player_two_type).to eq(:computer)
     end
   end
