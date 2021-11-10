4b88ffec64d4be5b771ac6017fede3e774fbb9fa
Tyler Decker <c_tydecker@groupon.com>
2021-11-10 08:58:53 -0500

    Questions .yes_or_no? .multichoice
    Questions.yes_or_no?  (string) -> bool
    takes a question and asks the users yes or no.
    It reruns true or flase
    
    Questions.multichoice (string, array) -> item from array
    taks a questions and asks the user to pick one of the items in the array
    by number. It then returns the item in the array

--- lib/questions.rb
+++ lib/questions.rb
@@ -8,7 +8,43 @@ class Questions
       else
         puts "INVALID OPTION"
         validate_user_input(question, valid_options)
-      end 
+      end
   end
 
+  def pick_a_number(question, valid_numbers)
+    while true do
+      puts question
+      print "( "
+      valid_numbers.each_with_index do | number, index |
+        print "#{number}"
+        print ", " unless index == valid_numbers.count - 1
+      end
+      puts " )"
+      user_answer = $stdin.gets.chomp.to_i
+      return user_answer if valid_numbers.include?(user_answer)
+      puts "INVALID OPTION"
+    end
+  end
+
+  def multichoice(question, valid_options)
+    while true do
+      puts question
+      valid_options.each_with_index do | option, index |
+        puts "#{index + 1} : #{option}"
+      end
+      user_answer = $stdin.gets.chomp.to_i - 1
+      return valid_options[user_answer] if user_answer > -1 && user_answer < valid_options.count
+      puts "INVALID OPTION"
+    end
+  end
+
+  def yes_or_no?(question)
+    while true do
+      $stdout.puts question
+      $stdout.puts "1. Yes"
+      $stdout.puts "2. No"
+      input = $stdin.gets.chomp.to_i
+      return 1 == input if input == 1 || input == 2
+    end
+  end
 end
--- spec/questions_spec.rb
+++ spec/questions_spec.rb
@@ -2,8 +2,8 @@ require 'questions'
 
 RSpec.describe Questions do
 
-describe "#validate_user_input" do
-  it "returns the value the user picked and change to integer" do
+  describe "#validate_user_input" do
+    it "returns the value the user picked and change to integer" do
       questions = Questions.new
 
       allow(STDIN).to receive(:gets).and_return("1\n")
@@ -11,10 +11,10 @@ describe "#validate_user_input" do
       answer = questions.validate_user_input("PICK A SPACE ", ["1", "2", "3", "4", "5", "6", "7", "8", "9"])
 
       expect(answer).to eq(1)
-  end
+    end
 
-  it "it loops asking the question until it gets a valid answer" do
-      questions = Questions.new  
+    it "it loops asking the question until it gets a valid answer" do
+      questions = Questions.new
       question = "PICK A SPACE "
       options = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
@@ -25,7 +25,110 @@ describe "#validate_user_input" do
       answer = questions.validate_user_input(question, options)
 
       expect(answer).to eq(1)
+    end
   end
-end
 
+  describe "#pick_a_number" do
+    it "returns the value the user picked and change as an integer" do
+      questions = Questions.new
+      question = "PICK A SPACE"
+
+      allow(STDIN).to receive(:gets).and_return("2\n")
+      expect(questions).to receive(:puts).once.with(question)
+      expect(questions).to receive(:print).once.with("( ")
+      expect(questions).to receive(:print).once.with("2")
+      expect(questions).to receive(:print).once.with(", ")
+      expect(questions).to receive(:print).once.with("3")
+      expect(questions).to receive(:puts).once.with(" )")
+
+      answer = questions.pick_a_number(question, [2, 3])
+
+      expect(answer).to eq(2)
+    end
+
+    it "it loops asking the question until it gets a valid answer" do
+      questions = Questions.new
+      question = "PICK A SPACE"
+      allow(STDIN).to receive(:gets).and_return("4\n", "3\n")
+      expect(questions).to receive(:puts).twice.with(question)
+      expect(questions).to receive(:print).twice.with("( ")
+      expect(questions).to receive(:print).twice.with("2")
+      expect(questions).to receive(:print).twice.with(", ")
+      expect(questions).to receive(:print).twice.with("3")
+      expect(questions).to receive(:puts).twice.with(" )")
+      expect(questions).to receive(:puts).with("INVALID OPTION")
+
+      answer = questions.pick_a_number(question, [2, 3])
+
+      expect(answer).to eq(3)
+    end
+  end
+
+  describe "#multichoice" do
+    it "returns the value the users picks" do
+      questions = Questions.new
+
+      expect(STDOUT).to receive(:puts).with("Pick one of these:")
+      expect(STDOUT).to receive(:puts).with("1 : first")
+      expect(STDOUT).to receive(:puts).with("2 : second")
+      expect(STDOUT).to receive(:puts).with("3 : third")
+
+      allow(STDIN).to receive(:gets).and_return("1\n")
+
+      answer = questions.multichoice("Pick one of these:", [:first, :second, :third])
+
+      expect(answer).to eq(:first)
+    end
+
+    it "it loops asking the question until it gets a valid answer" do
+      questions = Questions.new
+
+      expect(STDOUT).to receive(:puts).with("Pick one of these:").exactly(3).times
+      expect(STDOUT).to receive(:puts).with("1 : first").exactly(3).times
+      expect(STDOUT).to receive(:puts).with("2 : second").exactly(3).times
+      expect(STDOUT).to receive(:puts).with("3 : third").exactly(3).times
+      expect(STDOUT).to receive(:puts).with("INVALID OPTION").twice
+
+      allow(STDIN).to receive(:gets).and_return("0\n", "4\n", "3\n")
+
+      answer = questions.multichoice("Pick one of these:", [:first, :second, :third])
+
+      expect(answer).to eq(:third)
+    end
+  end
+
+  describe "#yes_or_no?" do
+    it "returns true when user picks yes" do
+      questions = Questions.new
+
+      expect(STDOUT).to receive(:puts).with("does this test pass?")
+      expect(STDOUT).to receive(:puts).with("1. Yes")
+      expect(STDOUT).to receive(:puts).with("2. No")
+      allow(STDIN).to receive(:gets).and_return("1\n")
+
+      expect(questions.yes_or_no?("does this test pass?")).to be true
+    end
+
+    it "returns false when user picks no" do
+      questions = Questions.new
+
+      expect(STDOUT).to receive(:puts).with("does this test pass?")
+      expect(STDOUT).to receive(:puts).with("1. Yes")
+      expect(STDOUT).to receive(:puts).with("2. No")
+      allow(STDIN).to receive(:gets).and_return("2\n")
+
+      expect(questions.yes_or_no?("does this test pass?")).to be false
+    end
+
+    it "it loops asking the question until it gets a valid answer" do
+      questions = Questions.new
+
+      expect(STDOUT).to receive(:puts).with("does this test pass?").twice
+      expect(STDOUT).to receive(:puts).with("1. Yes").twice
+      expect(STDOUT).to receive(:puts).with("2. No").twice
+      expect(STDIN).to receive(:gets).and_return("3\n", "1\n").twice
+
+      expect(questions.yes_or_no?("does this test pass?")).to be true
+    end
+  end
 end
