ef55804d8c0ca32c1bc6e8549826deada5b8e61b
Tyler Decker <c_tydecker@groupon.com>
2021-11-10 17:07:02 -0500

    remove questions code and use questions gem

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM ruby:3.0.0-alpine
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 COPY . /usr/src/app
-
+RUN bundle install
 CMD ["ruby", "bin/tictactoe.rb"]
--- bin/tictactoe.rb
+++ bin/tictactoe.rb
@@ -1,7 +1,10 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'bundler/setup'
+require "cli_questions"
 require_relative "../lib/game_loop"
 require_relative "../lib/env_var_parser"
 require_relative "../lib/game_logic"
-require_relative "../lib/questions"
 require_relative "../lib/console_output"
 require_relative "../lib/game_config"
 require_relative "../lib/computer_logic/computer_logic_easy"
@@ -10,7 +13,7 @@ require_relative "../lib/player_type/player_cpu"
 require_relative "../lib/player_type/player_human"
 
 parsed_env_vars = EnvVarParser.new()
-questions = Questions.new
+questions = Questions
 game_config = GameConfig.new(questions, parsed_env_vars)
 game_logic = GameLogic.new
 console_output = ConsoleOutput.new(game_config)
